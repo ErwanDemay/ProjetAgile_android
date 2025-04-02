8baf0a38ad4a539df39a597c74a5a854357c6dc3
VictorLabeille <courssiovm@gmail.com>
2025-04-02 16:12:05 +0200

    Main,Connexion et Proposition's Intent OK

--- app/src/main/java/com/example/projetagile/controleur/sessions/ConsultSessionActivity.java
+++ app/src/main/java/com/example/projetagile/controleur/sessions/ConsultSessionActivity.java
@@ -2,15 +2,37 @@ package com.example.projetagile.controleur.sessions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 import com.example.projetagile.R;
+import com.example.projetagile.controleur.PropositionActivity;
+
 
 public class ConsultSessionActivity extends AppCompatActivity {
-
+private Button buttonAjouter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consult_session);
+
+        buttonAjouter = findViewById(R.id.buttonAjouter);
+
+        buttonAjouter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ouvrirAjoutSession();
+            }
+        });
+    }
+
+    protected void ouvrirAjoutSession() {
+        //Log.d("debuG", "ouvrirAjoutSession: ");
+        Intent intent = new Intent(this, AjoutSessionActivity.class);
+        startActivity(intent);
     }
 }
